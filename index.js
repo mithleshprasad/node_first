de1f447c0215a1d1fcd93c6fbfe60f6185b44e57
mithlesh prasad <101449591+mithleshprasad@users.noreply.github.com>
2024-02-16 13:49:23 +0530

    Update index.js and add output.txt

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const fs = require("fs");
 console.warn("hello world");
 
-const textin = fs.readFileSync("./input.txt").toString(); // Convert buffer to string
+const textin = fs.readFileSync("./input.txt").toString();
+
 console.warn(textin);
+
+const txtout = `hello everyone mithlesh this side this is my 1st node project : ${textin} \n createdon ${Date.now()}`;
+
+fs.writeFileSync("./output.txt", txtout);
+
+console.warn(txtout);
